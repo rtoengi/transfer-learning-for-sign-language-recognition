df6991a9b57022e2df1a88002c9cbee0dc79bf1a
Roman Töngi <roman.toengi@gmail.com>
2020-05-22 14:24:01 +0200

    Comment the constants.

--- datasets/constants.py
+++ datasets/constants.py
@@ -1,5 +1,11 @@
+# Root directory of the `MS-ASL` dataset
 _MSASL_DIR = 'D:/datasets/msasl'
+
+# Directory of the `MS-ASL` dataset specification files
 _MSASL_SPECS_DIR = f'{_MSASL_DIR}/specs'
+
+# Directory of the `MS-ASL` downloaded YouTube videos
 _MSASL_VIDEOS_DIR = f'{_MSASL_DIR}/videos'
 
+# List of the train, validation and test dataset names
 DATASET_NAMES = ['train', 'val', 'test']
