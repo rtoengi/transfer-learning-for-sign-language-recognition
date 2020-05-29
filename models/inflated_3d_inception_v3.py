e2689dd4a19b512d8b679fffab86a2f229cd286a
Roman Töngi <roman.toengi@gmail.com>
2020-05-29 16:28:22 +0200

    Inflate the original Inception v3 model to add a temporal dimension.

--- models/inflated_3d_inception_v3.py
+++ models/inflated_3d_inception_v3.py
@@ -15,182 +15,183 @@
 from tensorflow.python.keras import layers
 from tensorflow.python.keras.engine import training
 
-_CHANNEL_AXIS = 3
+_CHANNEL_AXIS = 4
 
 
-def InceptionV3(input_shape=None, classes=1000):
+def Inflated3DInceptionV3(input_shape=(20, 224, 224, 3), classes=1000):
     inputs = layers.Input(shape=input_shape)
 
-    x = conv2d_bn(inputs, 32, 3, 3, strides=(2, 2), padding='valid')
-    x = conv2d_bn(x, 32, 3, 3, padding='valid')
-    x = conv2d_bn(x, 64, 3, 3)
-    x = layers.MaxPooling2D((3, 3), strides=(2, 2))(x)
+    x = conv3d_bn(inputs, 32, 3, 3, 3, strides=(1, 2, 2), padding='valid')
+    x = conv3d_bn(x, 32, 3, 3, 3, padding='valid')
+    x = conv3d_bn(x, 64, 3, 3, 3)
+    x = layers.MaxPooling3D((3, 3, 3), strides=(1, 2, 2))(x)
 
-    x = conv2d_bn(x, 80, 1, 1, padding='valid')
-    x = conv2d_bn(x, 192, 3, 3, padding='valid')
-    x = layers.MaxPooling2D((3, 3), strides=(2, 2))(x)
+    x = conv3d_bn(x, 80, 1, 1, 1, padding='valid')
+    x = conv3d_bn(x, 192, 3, 3, 3, padding='valid')
+    x = layers.MaxPooling3D((3, 3, 3), strides=(1, 2, 2))(x)
 
     # mixed 0: 35 x 35 x 256
-    branch1x1 = conv2d_bn(x, 64, 1, 1)
+    branch1x1 = conv3d_bn(x, 64, 1, 1, 1)
 
-    branch5x5 = conv2d_bn(x, 48, 1, 1)
-    branch5x5 = conv2d_bn(branch5x5, 64, 5, 5)
+    branch5x5 = conv3d_bn(x, 48, 1, 1, 1)
+    branch5x5 = conv3d_bn(branch5x5, 64, 5, 5, 5)
 
-    branch3x3dbl = conv2d_bn(x, 64, 1, 1)
-    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
-    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
+    branch3x3dbl = conv3d_bn(x, 64, 1, 1, 1)
+    branch3x3dbl = conv3d_bn(branch3x3dbl, 96, 3, 3, 3)
+    branch3x3dbl = conv3d_bn(branch3x3dbl, 96, 3, 3, 3)
 
-    branch_pool = layers.AveragePooling2D((3, 3), strides=(1, 1), padding='same')(x)
-    branch_pool = conv2d_bn(branch_pool, 32, 1, 1)
+    branch_pool = layers.AveragePooling3D((3, 3, 3), strides=(1, 1, 1), padding='same')(x)
+    branch_pool = conv3d_bn(branch_pool, 32, 1, 1, 1)
     x = layers.concatenate([branch1x1, branch5x5, branch3x3dbl, branch_pool], axis=_CHANNEL_AXIS, name='mixed0')
 
     # mixed 1: 35 x 35 x 288
-    branch1x1 = conv2d_bn(x, 64, 1, 1)
+    branch1x1 = conv3d_bn(x, 64, 1, 1, 1)
 
-    branch5x5 = conv2d_bn(x, 48, 1, 1)
-    branch5x5 = conv2d_bn(branch5x5, 64, 5, 5)
+    branch5x5 = conv3d_bn(x, 48, 1, 1, 1)
+    branch5x5 = conv3d_bn(branch5x5, 64, 5, 5, 5)
 
-    branch3x3dbl = conv2d_bn(x, 64, 1, 1)
-    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
-    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
+    branch3x3dbl = conv3d_bn(x, 64, 1, 1, 1)
+    branch3x3dbl = conv3d_bn(branch3x3dbl, 96, 3, 3, 3)
+    branch3x3dbl = conv3d_bn(branch3x3dbl, 96, 3, 3, 3)
 
-    branch_pool = layers.AveragePooling2D((3, 3), strides=(1, 1), padding='same')(x)
-    branch_pool = conv2d_bn(branch_pool, 64, 1, 1)
+    branch_pool = layers.AveragePooling3D((3, 3, 3), strides=(1, 1, 1), padding='same')(x)
+    branch_pool = conv3d_bn(branch_pool, 64, 1, 1, 1)
     x = layers.concatenate([branch1x1, branch5x5, branch3x3dbl, branch_pool], axis=_CHANNEL_AXIS, name='mixed1')
 
     # mixed 2: 35 x 35 x 288
-    branch1x1 = conv2d_bn(x, 64, 1, 1)
+    branch1x1 = conv3d_bn(x, 64, 1, 1, 1)
 
-    branch5x5 = conv2d_bn(x, 48, 1, 1)
-    branch5x5 = conv2d_bn(branch5x5, 64, 5, 5)
+    branch5x5 = conv3d_bn(x, 48, 1, 1, 1)
+    branch5x5 = conv3d_bn(branch5x5, 64, 5, 5, 5)
 
-    branch3x3dbl = conv2d_bn(x, 64, 1, 1)
-    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
-    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
+    branch3x3dbl = conv3d_bn(x, 64, 1, 1, 1)
+    branch3x3dbl = conv3d_bn(branch3x3dbl, 96, 3, 3, 3)
+    branch3x3dbl = conv3d_bn(branch3x3dbl, 96, 3, 3, 3)
 
-    branch_pool = layers.AveragePooling2D((3, 3), strides=(1, 1), padding='same')(x)
-    branch_pool = conv2d_bn(branch_pool, 64, 1, 1)
+    branch_pool = layers.AveragePooling3D((3, 3, 3), strides=(1, 1, 1), padding='same')(x)
+    branch_pool = conv3d_bn(branch_pool, 64, 1, 1, 1)
     x = layers.concatenate([branch1x1, branch5x5, branch3x3dbl, branch_pool], axis=_CHANNEL_AXIS, name='mixed2')
 
     # mixed 3: 17 x 17 x 768
-    branch3x3 = conv2d_bn(x, 384, 3, 3, strides=(2, 2), padding='valid')
+    branch3x3 = conv3d_bn(x, 384, 3, 3, 3, strides=(1, 2, 2), padding='valid')
 
-    branch3x3dbl = conv2d_bn(x, 64, 1, 1)
-    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
-    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3, strides=(2, 2), padding='valid')
+    branch3x3dbl = conv3d_bn(x, 64, 1, 1, 1)
+    branch3x3dbl = conv3d_bn(branch3x3dbl, 96, 3, 3, 3)
+    branch3x3dbl = conv3d_bn(branch3x3dbl, 96, 3, 3, 3, strides=(1, 2, 2), padding='valid')
 
-    branch_pool = layers.MaxPooling2D((3, 3), strides=(2, 2))(x)
+    branch_pool = layers.MaxPooling3D((3, 3, 3), strides=(1, 2, 2))(x)
     x = layers.concatenate([branch3x3, branch3x3dbl, branch_pool], axis=_CHANNEL_AXIS, name='mixed3')
 
     # mixed 4: 17 x 17 x 768
-    branch1x1 = conv2d_bn(x, 192, 1, 1)
+    branch1x1 = conv3d_bn(x, 192, 1, 1, 1)
 
-    branch7x7 = conv2d_bn(x, 128, 1, 1)
-    branch7x7 = conv2d_bn(branch7x7, 128, 1, 7)
-    branch7x7 = conv2d_bn(branch7x7, 192, 7, 1)
+    branch7x7 = conv3d_bn(x, 128, 1, 1, 1)
+    branch7x7 = conv3d_bn(branch7x7, 128, 1, 1, 7)
+    branch7x7 = conv3d_bn(branch7x7, 192, 1, 7, 1)
 
-    branch7x7dbl = conv2d_bn(x, 128, 1, 1)
-    branch7x7dbl = conv2d_bn(branch7x7dbl, 128, 7, 1)
-    branch7x7dbl = conv2d_bn(branch7x7dbl, 128, 1, 7)
-    branch7x7dbl = conv2d_bn(branch7x7dbl, 128, 7, 1)
-    branch7x7dbl = conv2d_bn(branch7x7dbl, 192, 1, 7)
+    branch7x7dbl = conv3d_bn(x, 128, 1, 1, 1)
+    branch7x7dbl = conv3d_bn(branch7x7dbl, 128, 1, 7, 1)
+    branch7x7dbl = conv3d_bn(branch7x7dbl, 128, 1, 1, 7)
+    branch7x7dbl = conv3d_bn(branch7x7dbl, 128, 1, 7, 1)
+    branch7x7dbl = conv3d_bn(branch7x7dbl, 192, 1, 1, 7)
 
-    branch_pool = layers.AveragePooling2D((3, 3), strides=(1, 1), padding='same')(x)
-    branch_pool = conv2d_bn(branch_pool, 192, 1, 1)
+    branch_pool = layers.AveragePooling3D((3, 3, 3), strides=(1, 1, 1), padding='same')(x)
+    branch_pool = conv3d_bn(branch_pool, 192, 1, 1, 1)
     x = layers.concatenate([branch1x1, branch7x7, branch7x7dbl, branch_pool], axis=_CHANNEL_AXIS, name='mixed4')
 
     # mixed 5, 6: 17 x 17 x 768
     for i in range(2):
-        branch1x1 = conv2d_bn(x, 192, 1, 1)
+        branch1x1 = conv3d_bn(x, 192, 1, 1, 1)
 
-        branch7x7 = conv2d_bn(x, 160, 1, 1)
-        branch7x7 = conv2d_bn(branch7x7, 160, 1, 7)
-        branch7x7 = conv2d_bn(branch7x7, 192, 7, 1)
+        branch7x7 = conv3d_bn(x, 160, 1, 1, 1)
+        branch7x7 = conv3d_bn(branch7x7, 160, 1, 1, 7)
+        branch7x7 = conv3d_bn(branch7x7, 192, 1, 7, 1)
 
-        branch7x7dbl = conv2d_bn(x, 160, 1, 1)
-        branch7x7dbl = conv2d_bn(branch7x7dbl, 160, 7, 1)
-        branch7x7dbl = conv2d_bn(branch7x7dbl, 160, 1, 7)
-        branch7x7dbl = conv2d_bn(branch7x7dbl, 160, 7, 1)
-        branch7x7dbl = conv2d_bn(branch7x7dbl, 192, 1, 7)
+        branch7x7dbl = conv3d_bn(x, 160, 1, 1, 1)
+        branch7x7dbl = conv3d_bn(branch7x7dbl, 160, 1, 7, 1)
+        branch7x7dbl = conv3d_bn(branch7x7dbl, 160, 1, 1, 7)
+        branch7x7dbl = conv3d_bn(branch7x7dbl, 160, 1, 7, 1)
+        branch7x7dbl = conv3d_bn(branch7x7dbl, 192, 1, 1, 7)
 
-        branch_pool = layers.AveragePooling2D((3, 3), strides=(1, 1), padding='same')(x)
-        branch_pool = conv2d_bn(branch_pool, 192, 1, 1)
+        branch_pool = layers.AveragePooling3D((3, 3, 3), strides=(1, 1, 1), padding='same')(x)
+        branch_pool = conv3d_bn(branch_pool, 192, 1, 1, 1)
         x = layers.concatenate([branch1x1, branch7x7, branch7x7dbl, branch_pool], axis=_CHANNEL_AXIS,
                                name=f'mixed{5 + i}')
 
     # mixed 7: 17 x 17 x 768
-    branch1x1 = conv2d_bn(x, 192, 1, 1)
+    branch1x1 = conv3d_bn(x, 192, 1, 1, 1)
 
-    branch7x7 = conv2d_bn(x, 192, 1, 1)
-    branch7x7 = conv2d_bn(branch7x7, 192, 1, 7)
-    branch7x7 = conv2d_bn(branch7x7, 192, 7, 1)
+    branch7x7 = conv3d_bn(x, 192, 1, 1, 1)
+    branch7x7 = conv3d_bn(branch7x7, 192, 1, 1, 7)
+    branch7x7 = conv3d_bn(branch7x7, 192, 1, 7, 1)
 
-    branch7x7dbl = conv2d_bn(x, 192, 1, 1)
-    branch7x7dbl = conv2d_bn(branch7x7dbl, 192, 7, 1)
-    branch7x7dbl = conv2d_bn(branch7x7dbl, 192, 1, 7)
-    branch7x7dbl = conv2d_bn(branch7x7dbl, 192, 7, 1)
-    branch7x7dbl = conv2d_bn(branch7x7dbl, 192, 1, 7)
+    branch7x7dbl = conv3d_bn(x, 192, 1, 1, 1)
+    branch7x7dbl = conv3d_bn(branch7x7dbl, 192, 1, 7, 1)
+    branch7x7dbl = conv3d_bn(branch7x7dbl, 192, 1, 1, 7)
+    branch7x7dbl = conv3d_bn(branch7x7dbl, 192, 1, 7, 1)
+    branch7x7dbl = conv3d_bn(branch7x7dbl, 192, 1, 1, 7)
 
-    branch_pool = layers.AveragePooling2D((3, 3), strides=(1, 1), padding='same')(x)
-    branch_pool = conv2d_bn(branch_pool, 192, 1, 1)
+    branch_pool = layers.AveragePooling3D((3, 3, 3), strides=(1, 1, 1), padding='same')(x)
+    branch_pool = conv3d_bn(branch_pool, 192, 1, 1, 1)
     x = layers.concatenate([branch1x1, branch7x7, branch7x7dbl, branch_pool], axis=_CHANNEL_AXIS, name='mixed7')
 
     # mixed 8: 8 x 8 x 1280
-    branch3x3 = conv2d_bn(x, 192, 1, 1)
-    branch3x3 = conv2d_bn(branch3x3, 320, 3, 3, strides=(2, 2), padding='valid')
+    branch3x3 = conv3d_bn(x, 192, 1, 1, 1)
+    branch3x3 = conv3d_bn(branch3x3, 320, 3, 3, 3, strides=(1, 2, 2), padding='valid')
 
-    branch7x7x3 = conv2d_bn(x, 192, 1, 1)
-    branch7x7x3 = conv2d_bn(branch7x7x3, 192, 1, 7)
-    branch7x7x3 = conv2d_bn(branch7x7x3, 192, 7, 1)
-    branch7x7x3 = conv2d_bn(branch7x7x3, 192, 3, 3, strides=(2, 2), padding='valid')
+    branch7x7x3 = conv3d_bn(x, 192, 1, 1, 1)
+    branch7x7x3 = conv3d_bn(branch7x7x3, 192, 1, 1, 7)
+    branch7x7x3 = conv3d_bn(branch7x7x3, 192, 1, 7, 1)
+    branch7x7x3 = conv3d_bn(branch7x7x3, 192, 3, 3, 3, strides=(1, 2, 2), padding='valid')
 
-    branch_pool = layers.MaxPooling2D((3, 3), strides=(2, 2))(x)
+    branch_pool = layers.MaxPooling3D((3, 3, 3), strides=(1, 2, 2))(x)
     x = layers.concatenate([branch3x3, branch7x7x3, branch_pool], axis=_CHANNEL_AXIS, name='mixed8')
 
     # mixed 9: 8 x 8 x 2048
     for i in range(2):
-        branch1x1 = conv2d_bn(x, 320, 1, 1)
+        branch1x1 = conv3d_bn(x, 320, 1, 1, 1)
 
-        branch3x3 = conv2d_bn(x, 384, 1, 1)
-        branch3x3_1 = conv2d_bn(branch3x3, 384, 1, 3)
-        branch3x3_2 = conv2d_bn(branch3x3, 384, 3, 1)
+        branch3x3 = conv3d_bn(x, 384, 1, 1, 1)
+        branch3x3_1 = conv3d_bn(branch3x3, 384, 1, 1, 3)
+        branch3x3_2 = conv3d_bn(branch3x3, 384, 1, 3, 1)
         branch3x3 = layers.concatenate([branch3x3_1, branch3x3_2], axis=_CHANNEL_AXIS, name=f'mixed9_{i}')
 
-        branch3x3dbl = conv2d_bn(x, 448, 1, 1)
-        branch3x3dbl = conv2d_bn(branch3x3dbl, 384, 3, 3)
-        branch3x3dbl_1 = conv2d_bn(branch3x3dbl, 384, 1, 3)
-        branch3x3dbl_2 = conv2d_bn(branch3x3dbl, 384, 3, 1)
+        branch3x3dbl = conv3d_bn(x, 448, 1, 1, 1)
+        branch3x3dbl = conv3d_bn(branch3x3dbl, 384, 3, 3, 3)
+        branch3x3dbl_1 = conv3d_bn(branch3x3dbl, 384, 1, 1, 3)
+        branch3x3dbl_2 = conv3d_bn(branch3x3dbl, 384, 1, 3, 1)
         branch3x3dbl = layers.concatenate([branch3x3dbl_1, branch3x3dbl_2], axis=_CHANNEL_AXIS)
 
-        branch_pool = layers.AveragePooling2D((3, 3), strides=(1, 1), padding='same')(x)
-        branch_pool = conv2d_bn(branch_pool, 192, 1, 1)
+        branch_pool = layers.AveragePooling3D((3, 3, 3), strides=(1, 1, 1), padding='same')(x)
+        branch_pool = conv3d_bn(branch_pool, 192, 1, 1, 1)
         x = layers.concatenate([branch1x1, branch3x3, branch3x3dbl, branch_pool], axis=_CHANNEL_AXIS,
                                name=f'mixed{9 + i}')
 
     # Classification block
-    x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
+    x = layers.GlobalAveragePooling3D(name='avg_pool')(x)
     x = layers.Dense(classes, activation='softmax', name='predictions')(x)
 
     # Create model
-    model = training.Model(inputs, x, name='inception_v3')
+    model = training.Model(inputs, x, name='inflated_3d_inception_v3')
     return model
 
 
-def conv2d_bn(x, filters, num_row, num_col, padding='same', strides=(1, 1), name=None):
+def conv3d_bn(x, filters, num_step, num_row, num_col, padding='same', strides=(1, 1, 1), name=None):
     """Utility function to apply convolution and batch normalization.
 
     Arguments:
         x: The input tensor.
-        filters: The filters in `Conv2D`.
+        filters: The filters in `Conv3D`.
+        num_step: The depth of the convolution kernel.
         num_row: The height of the convolution kernel.
         num_col: The width of the convolution kernel.
-        padding: The padding mode in `Conv2D`.
-        strides: The strides in `Conv2D`.
+        padding: The padding mode in `Conv3D`.
+        strides: The strides in `Conv3D`.
         name: The name of the ops; will become `name + '_conv'` for the convolution and `name + '_bn'` for the batch
               norm layer.
 
     Returns:
-        The output tensor after applying `Conv2D` and `BatchNormalization`.
+        The output tensor after applying `Conv3D` and `BatchNormalization`.
     """
     if name is not None:
         bn_name = name + '_bn'
@@ -199,7 +200,8 @@ def conv2d_bn(x, filters, num_row, num_col, padding='same', strides=(1, 1), name
         bn_name = None
         conv_name = None
 
-    x = layers.Conv2D(filters, (num_row, num_col), strides=strides, padding=padding, use_bias=False, name=conv_name)(x)
+    x = layers.Conv3D(filters, (num_step, num_row, num_col), strides=strides, padding=padding, use_bias=False,
+                      name=conv_name)(x)
     x = layers.BatchNormalization(axis=_CHANNEL_AXIS, scale=False, name=bn_name)(x)
     x = layers.Activation('relu', name=name)(x)
 
