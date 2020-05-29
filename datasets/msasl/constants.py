87bbd5aac5d7c498e334149127ec57217bd17709
Roman Töngi <roman.toengi@gmail.com>
2020-05-29 22:22:42 +0200

    Reword comment.

--- datasets/msasl/constants.py
+++ datasets/msasl/constants.py
@@ -10,7 +10,7 @@ _MSASL_SPECS_DIR = f'{_MSASL_DIR}/specs'
 # Directory of the filtered `MS-ASL` dataset specification files
 _MSASL_FILTERED_SPECS_DIR = f'{_MSASL_DIR}/filtered_specs'
 
-# Directory of the `MS-ASL` downloaded YouTube videos
+# Directory of the downloaded `MS-ASL` YouTube videos
 _MSASL_VIDEOS_DIR = f'{_MSASL_DIR}/videos'
 
 # Directory of the `MS-ASL` train, validation and test `TFRecord` files
