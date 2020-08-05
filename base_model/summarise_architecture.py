54aa6deab6f6c4e5501aaba3fbc5188b06365a49
Roman Töngi <roman.toengi@gmail.com>
2020-08-05 22:06:23 +0200

    Adjust the column widths of the summary output.

--- base_model/summarise_architecture.py
+++ base_model/summarise_architecture.py
@@ -5,7 +5,7 @@ from base_model.inflated_3d_inception_v3 import Inflated3DInceptionV3
 
 def summarise_architecture():
     model = Inflated3DInceptionV3()
-    model.summary(line_length=120, positions=[.4, .65, .75, 1.])
+    model.summary(line_length=90, positions=[.25, .57, .7, 1.])
     plot_model(model, 'model_architecture.png', show_shapes=True)
 
 
